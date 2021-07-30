3e2e70c2f853d878b787411a5a2780d6c3f1ae18
Carlos Afonso <cafonsop@amazon.com>
2021-07-30 17:05:34 +0000

    Implement scheduled Lambda function that constantly inserts data into the index

--- graviton2/elasticsearch_graviton/es.py
+++ graviton2/elasticsearch_graviton/es.py
@@ -3,7 +3,11 @@
 from aws_cdk import core
 import aws_cdk.aws_ec2 as ec2
 import aws_cdk.aws_elasticsearch as es
+import aws_cdk.aws_events as events
+import aws_cdk.aws_events_targets as events_targets
 import aws_cdk.aws_iam as iam
+import aws_cdk.aws_lambda as _lambda
+from aws_cdk.aws_lambda_python import PythonFunction
 import os
 
 c9_ip = os.environ["C9_HOSTNAME"] + '/32'
@@ -32,5 +36,23 @@ class CdkElasticsearchStack(core.Stack):
                               access_policies=[access_policy])
         es_domain.connections.allow_from_any_ipv4(ec2.Port.all_traffic())
 
-        core.CfnOutput(self, "DomainEndpoint", value=es_domain. domain_endpoint)
+        insert_fn = PythonFunction(self,
+                       "InsertIntoIndexFunction",
+                       entry="graviton2/elasticsearch_graviton/lambdas",
+                       index="insert_into_index.py",
+                       handler="handler",
+                       runtime=_lambda.Runtime.PYTHON_3_8,
+                       vpc=vpc,
+                       environment={
+                           "ES_ENDPOINT": es_domain.domain_endpoint
+                       },
+                       timeout=core.Duration.minutes(1))
+        es_domain.grant_write(insert_fn.grant_principal)
 
+        scheduled_rule = events.Rule(self,
+                                      "ScheduledIndexInsertionRule",
+                                      schedule=events.Schedule.expression('cron(* * ? * * *)'))
+        scheduled_rule.add_target(events_targets.LambdaFunction(insert_fn))
+
+        core.CfnOutput(self, "DomainEndpoint", value=es_domain.domain_endpoint)
+        core.CfnOutput(self, "DomainName", value=es_domain.domain_name)
---
+++ graviton2/elasticsearch_graviton/lambdas/insert_into_index.py
@@ -0,0 +1,54 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved. SPDX-License-Identifier: MIT-0
+#
+# This lambda function inserts random data into an index named "people" within 
+# an Amazon Elasticsearch cluster specified by the user. The function will do
+# several insertions before exiting.
+
+import argparse
+import os
+import sys
+import time
+
+
+import boto3
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from elasticsearch.exceptions import ConnectionTimeout
+from faker import Faker
+from requests_aws4auth import AWS4Auth
+
+
+endpoint = os.environ['ES_ENDPOINT']
+region = os.environ['AWS_REGION']
+
+
+def handler(event, context):
+    service = 'es'
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+    
+    fake = Faker()
+    
+    es = Elasticsearch(
+        hosts = [{'host': endpoint, 'port': 443}],
+        http_auth = awsauth,
+        use_ssl = True,
+        verify_certs = True,
+        connection_class = RequestsHttpConnection
+    )
+    
+    attempts = 20
+    print("Will insert %s items into the index" % attempts)
+    for i in range(attempts):
+        document = fake.profile()
+        
+        try:
+            result = es.index(index="people", doc_type="_doc", body=document)
+            print("Indexed with ID '%s'" % result['_id'])
+    
+        except ConnectionTimeout as e:
+            print("Connection to the ES cluster timed out: %s" % str(e))
+        
+        except Exception as e:
+            print("Unexpected exception caught: %s" % str(e))
+        
+        time.sleep(2)
