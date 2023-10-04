cafdc8fd401d7ef1c8583810586fda6392d6d3cb
Bryan Furlong <furlonb@gmail.com>
2023-10-04 18:30:51 +0000

    Upgraded ES to OS

--- graviton2/opensearch_graviton/open_search.py
+++ graviton2/opensearch_graviton/open_search.py
@@ -3,7 +3,7 @@
 import aws_cdk as cdk
 from constructs import Construct
 import aws_cdk.aws_ec2 as ec2
-import aws_cdk.aws_elasticsearch as es
+import aws_cdk.aws_opensearchservice as open_search
 import aws_cdk.aws_events as events
 import aws_cdk.aws_events_targets as events_targets
 import aws_cdk.aws_iam as iam
@@ -13,7 +13,7 @@ import os
 
 c9_ip = os.environ["C9_HOSTNAME"] + '/32'
 
-class CdkElasticsearchStack(cdk.Stack):
+class CdkOpenSearchStack(cdk.Stack):
     """A stack containing a basic Amazon Elasticsearch domain running on the
     x86 architecture."""
 
@@ -34,17 +34,17 @@ class CdkElasticsearchStack(cdk.Stack):
             ec2.Peer.ipv4(c9_ip),
             ec2.Port.all_traffic()
         )
-        es_domain = es.Domain(self,
+        es_domain = open_search.Domain(self,
                               "Domain",
-                              version=es.ElasticsearchVersion.V7_9,
+                              version=open_search.EngineVersion.OPENSEARCH_1_0,
                               vpc=vpc,
                               capacity={
-                                  'data_node_instance_type': 'm5.large.elasticsearch',
+                                  'data_node_instance_type': 'm5.large.search',
                                   'data_nodes': 2,
                                   'master_nodes': 0,
                                   'warm_nodes': 0
                               },
-                              zone_awareness= es.ZoneAwarenessConfig(
+                              zone_awareness= open_search.ZoneAwarenessConfig(
                                   availability_zone_count=2
                               ),
                               vpc_subnets=[ec2.SubnetSelection(subnet_type=ec2.SubnetType.PRIVATE_WITH_EGRESS)],
@@ -54,7 +54,7 @@ class CdkElasticsearchStack(cdk.Stack):
 
         insert_fn = PythonFunction(self,
                        "InsertIntoIndexFunction",
-                       entry="graviton2/elasticsearch_graviton/lambdas",
+                       entry="graviton2/opensearch_graviton/lambdas",
                        index="insert_into_index.py",
                        handler="handler",
                        runtime=_lambda.Runtime.PYTHON_3_8,
