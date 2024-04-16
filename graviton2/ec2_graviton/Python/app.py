1ff45526890303fb12a7a9eb7b598017117bde7b
“Jacob <jacov141@gmail.com>
2024-04-16 16:29:54 +0300

    some changes

--- graviton2/ec2_graviton/Python/app.py
+++ graviton2/ec2_graviton/Python/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import short_url
 from short_url import create_short_url, retrive_from_dynamo
+import boto3
 
 app = Flask(__name__)
 
@@ -17,6 +18,8 @@ def index():
 
 @app.route('/shortenURL', methods=['POST'])
 def short_url_post():
+    session = boto3.session.Session()
+    return session.region_name
     data = request.get_json()
     original_url = data.get('OriginalURL')  # Use the correct key from your JSON payload
     if not original_url:
