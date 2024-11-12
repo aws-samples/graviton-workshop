0fbf573fc5d1a85343ba5584b70a9ddaf5908a53
Marcin Bednarz <mbz@amazon.com>
2024-11-12 05:55:34 +0000

    ecs stack refactoring

--- graviton/cs_graviton/ecs_graviton.py
+++ graviton/cs_graviton/ecs_graviton.py
@@ -6,6 +6,8 @@ import aws_cdk.aws_ec2 as ec2
 import aws_cdk.aws_ecs as ecs
 import aws_cdk.aws_elasticloadbalancingv2 as elbv2
 import aws_cdk.aws_ssm as ssm
+import aws_cdk.aws_autoscaling as autoscaling
+import aws_cdk.aws_iam as iam
 import os
 
 
@@ -20,20 +22,68 @@ class CdkEcsStack(cdk.Stack):
             vpc=vpc,
             container_insights=True
         )
+        
+        ecs_security_group = ec2.SecurityGroup(
+            self, "EcsSecurityGroup",
+            vpc=vpc,
+            allow_all_outbound=True
+        )
+        ecs_security_group.add_ingress_rule(
+            ec2.Peer.ipv4('10.0.0.0/16'),
+            ec2.Port.all_traffic()
+        )
+        
+        ecs_role = iam.Role(
+            self,
+            "Ec2ExecutionRole",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
+            description="The instance's permissions (HOST of the container)",
+        )
 
+        ecs_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2ContainerServiceforEC2Role"))
+        ecs_role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedEC2InstanceDefaultPolicy"))
+    
+        ec2_user_data = ec2.UserData.for_linux() 
+
+        launch_template = ec2.LaunchTemplate(
+            self,
+            "LaunchTemplate",
+            instance_type=ec2.InstanceType("m6g.2xlarge"),
+            machine_image=ecs.EcsOptimizedImage.amazon_linux2023(ecs.AmiHardwareType.ARM),
+            user_data=ec2_user_data,
+            require_imdsv2=True,
+            role=ecs_role,
+            security_group=ecs_security_group
+        )
+
+        auto_scaling_group = autoscaling.AutoScalingGroup(
+            self,
+            "Asg",
+            vpc=vpc,
+            launch_template=launch_template,
+            min_capacity=1,
+            max_capacity=2,
+            new_instances_protected_from_scale_in=False,
+        )
+
+        capacity_provider = ecs.AsgCapacityProvider(
+            self,
+            "AsgCapacityProvider",
+            auto_scaling_group=auto_scaling_group,
+            enable_managed_termination_protection=False,
+            enable_managed_draining=False,
+        )
+        
+        cluster.add_asg_capacity_provider(capacity_provider)
+        cluster.add_default_capacity_provider_strategy([
+            ecs.CapacityProviderStrategy(capacity_provider=capacity_provider.capacity_provider_name)
+        ])
+        
         task_definition = ecs.Ec2TaskDefinition(
             self, "TaskDef")
 
         container_uri = ssm.StringParameter.value_for_string_parameter(self ,"graviton_lab_container_uri")
-
-        ecs_ami = ecs.EcsOptimizedImage.amazon_linux2(ecs.AmiHardwareType.ARM)
-
-        cluster.add_capacity("G2AutoScalingGroup",
-                         instance_type=ec2.InstanceType("m6g.2xlarge"),
-                         machine_image=ecs_ami
-
-        )
-
+        
         container = task_definition.add_container(
             "web",
             image=ecs.ContainerImage.from_registry(container_uri),
@@ -51,7 +101,7 @@ class CdkEcsStack(cdk.Stack):
         )
         port_mapping =  ecs.PortMapping(
             container_port=3000,
-            host_port=8080,
+            host_port=8888,
             protocol=ecs.Protocol.TCP
         )
 
@@ -61,7 +111,10 @@ class CdkEcsStack(cdk.Stack):
         service = ecs.Ec2Service(
             self, "Service",
             cluster=cluster,
-            task_definition=task_definition
+            task_definition=task_definition,
+            desired_count=1,
+            min_healthy_percent=0,
+            max_healthy_percent=100,
         )
 
         # Create ALB
